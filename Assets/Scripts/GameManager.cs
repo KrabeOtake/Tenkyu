cbe7c012471e80b38dc9640de5136485d71e42d0
KrabeOtake <cheshirekrab@gmail.com>
2018-10-29 03:23:18 +0300

    Add project files

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+private static GameManager Instance = null;
+public Canvas canvasUI;
+public Text text;
+
+	void Awake()
+	{
+	 if (Instance == null)
+			 Instance = this;
+	 else if(Instance == this)
+		 Destroy(gameObject);
+
+	 InitializeManager();
+	}
+
+	private void InitializeManager()
+	{
+    canvasUI = GameObject.FindWithTag("UI").GetComponent<Canvas>();
+		text = GameObject.FindWithTag("UIText").GetComponent<Text>();
+  }
+
+	public static void ShowWinUI()
+	{
+		Instance.canvasUI.gameObject.SetActive(true);
+		Instance.text.text = "Level complete!";
+		Instance.text.color = Color.green;
+	}
+
+	public static void ShowLoseUI()
+	{
+		Instance.canvasUI.gameObject.SetActive(true);
+		Instance.text.text = "Lose!";
+		Instance.text.color = Color.red;
+	}
+
+	public static void Restart()
+	{
+		Instance.StartCoroutine(Wait());
+	}
+
+	static IEnumerator Wait()
+	{
+		yield return new WaitForSeconds(3);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Hole"))
+		{
+			GameManager.Restart();
+			GameManager.ShowWinUI();
+			gameObject.SetActive(false);
+		}
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("RedBall"))
+		{
+			GameManager.Restart();
+			GameManager.ShowLoseUI();
+			gameObject.SetActive(false);
+		}
+	}
+}
